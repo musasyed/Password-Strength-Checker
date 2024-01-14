a059f1107eb6369514d582bf92bd1bed5514212b
Syed Musa Azam <musasyed16@gmail.com>
2024-01-15 03:21:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var password=document.getElementById("password");
+var message=document.getElementById("message");
+var strength=document.getElementById("strength");
+
+
+
+password.addEventListener("input",()=>{
+    if(password.value.length>0){
+        message.style.display="block";
+       
+    }
+    else{
+        message.style.display="none";
+    }
+
+    if(password.value.length<4){
+       strength.innerHTML="weak";
+       password.style.borderColor="#ff5925";
+       message.style.color="#ff5925"
+       
+    }
+    else if(password.value.length>=4 && password.value.length<8){
+        strength.innerHTML="Medium";
+        password.style.borderColor="yellow";
+        message.style.color="yellow"
+        
+     }
+     else if(password.value.length>=8){
+        strength.innerHTML="Strong";
+        password.style.borderColor="#26d730";
+        message.style.color="#26d730"
+        
+     }
+
+})
